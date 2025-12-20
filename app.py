c36772af826e046bbfb1ce2a5b22d091c5c67211
JM Bell <justinmbell1997@gmail.com>
2025-12-20 13:01:15 -0600

    General - added model class and updated UI

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import joblib
+from clickbait_model import ClickbaitClassifierModel
 
 app = Flask(__name__)
 
+model = joblib.load('finalized_model.pkl') # Update path as needed
+
 @app.route('/', methods=['GET'])
-def hello_world():
-    return "Hello World"
+def startPage():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    title = request.files['title']
+    model = ClickbaitClassifierModel()
+    prediction = ClickbaitClassifierModel.predict(title)
+    
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
